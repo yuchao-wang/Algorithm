d606a0140d793bbdb6bb6346002aa03f4c7e94b7
yuchao-wang <1154786190@qq.com>
2017-05-02 16:04:11 +0800

    tree

---
+++ src/wang/yuchao/algorithm/tree/BinaryTree.java
@@ -0,0 +1,11 @@
+package wang.yuchao.algorithm.tree;
+
+/**
+ * Created by wangyuchao on 17/4/27.
+ */
+public class BinaryTree {
+    public int data;
+    public BinaryTree left;
+    public BinaryTree right;
+
+}
