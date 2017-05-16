fde02f21b7dbd12d5e5d8e08d5549848f7c6ed4b
yuchao-wang <1154786190@qq.com>
2017-05-16 19:14:23 +0800

    leetcode

--- src/wang/yuchao/algorithm/leetcode/Solution_001.java
+++ src/wang/yuchao/algorithm/leetcode/Solution_001.java
@@ -1,30 +1,48 @@
 package wang.yuchao.algorithm.leetcode;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 
 /**
  * Created by wangyuchao on 17/5/16.
  */
 public class Solution_001 {
     public static int[] twoSum(int[] arrays, int target) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int i = 0; i < arrays.length; i++) {
-            hashMap.put(arrays[i], i);
-        }
-        for (int i = 0; i < arrays.length; i++) {
-            int temp = target - arrays[i];
-            if (hashMap.containsKey(temp)) {
-                int index1 = hashMap.get(temp);
-                int index2 = hashMap.get(arrays[i]);
-                return new int[]{index1, index2};
+        if (arrays != null && arrays.length >= 2) {
+            HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+            for (int i = 0; i < arrays.length; i++) {
+                int temp = target - arrays[i];
+                if (hashMap.containsKey(temp)) {
+                    int index = hashMap.get(temp);
+                    if (arrays[i] + arrays[index] == target) {
+                        return new int[]{index, i};
+                    }
+                } else {
+                    hashMap.put(arrays[i], i);
+                }
+            }
+            for (int i = 0; i < arrays.length; i++) {
+                int temp = target - arrays[i];
+                if (hashMap.containsKey(temp)) {
+                    int index1 = hashMap.get(temp);
+                    int index2 = hashMap.get(arrays[i]);
+                    if (index1 != index2) {
+                        if (index1 > index2) {
+                            return new int[]{index2, index1};
+                        } else {
+                            return new int[]{index1, index2};
+                        }
+                    }
+                }
             }
         }
         return null;
     }
 
     public static void main(String[] args) {
-        int arrays[] = {1, 3, 5, 7, 11, 13, 17};
-        int target = 28;
+//        int[] arrays = {3, 2, 4};
+        int[] arrays = {3, 3};
+        int target = 6;
         int[] result = twoSum(arrays, target);
         System.out.println(result[0] + "," + result[1]);
     }
